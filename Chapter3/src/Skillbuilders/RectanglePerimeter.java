06217fb03d457c8abd7ca3758727af3377ca8125
49118011 <49118011@L815WD220027.edu.cbe.ab.ca>
2025-09-11 10:07:45 -0600

    10:07am

--- Chapter3/src/Skillbuilders/RectanglePerimeter.java
+++ Chapter3/src/Skillbuilders/RectanglePerimeter.java
@@ -1,12 +1,28 @@
 package Skillbuilders;
 
+import java.util.Scanner;
+
 public class RectanglePerimeter 
 {
 
 	public static void main(String[] args) 
 	{
-// sam palmer
+	//declare variables	
+		 Scanner input = new Scanner(System.in);
+	        
+	        System.out.print("Enter the width of the rectangle: ");
+	        int width = input.nextInt();
+	        
+	        System.out.print("Enter the length of the rectangle: ");
+	        int length = input.nextInt();
+	        int area = width * length;  
+	        System.out.println("The surface area of the rectangle is: " + area);
+	        int perimeter = 2 * width + 2 * length;
+        System.out.println("The perimeter of the rectangle is: " + perimeter);
+        System.out.println("Width is: " + width);
+        System.out.println("Length is: " + length);
 	
+        input.close();
 	}
 
 }
